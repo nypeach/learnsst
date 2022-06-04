bf7156a21d7e184e8d9a5e30f4aa0a32b5b37a68
Jodi Silverman <jodimsilverman@gmail.com>
2022-06-04 12:11:39 -0400

    Securing the API

--- notes/backend/functions/create.js
+++ notes/backend/functions/create.js
@@ -2,7 +2,7 @@ import * as uuid from "uuid";
 import handler from "../util/handler";
 import dynamoDb from "../util/dynamodb";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
@@ -10,7 +10,7 @@ export const main = handler(async (event) => {
     TableName: process.env.TABLE_NAME,
     Item: {
       // The attributes of the item to be created
-      userId: "123", // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
